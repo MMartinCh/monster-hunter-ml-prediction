ae188833a89423e013e317c0af0b75f818562f1c
MMartinCh <Moritz.Martin.c@gmail.com>
2026-08-02 12:45:52 +0200

    RankingScraper refactored.

--- src/data_collection/scrapers/ranking_scraper.py
+++ src/data_collection/scrapers/ranking_scraper.py
@@ -1,54 +1,57 @@
 import itertools
 import logging
-import sys
+from functools import cached_property
 
+import pandas as pd
 from bs4 import BeautifulSoup
 from typing import List
 
-from src.core.dataclasses import RankingScraperItem
-from src.core.interfaces import AbstractWebScraper
+from src.core.dataclasses import RankingScraperItem #type:ignore
+from src.core.interfaces import AbstractWebScraper #type:ignore
+from src.core.helpers import file_cache #type:ignore
 
 logger = logging.getLogger(__name__)
 
 class RankingScraper(AbstractWebScraper[RankingScraperItem]):
     """Scrapes monster names and rankings from MH 20th anniversary website."""
-    def __init__(self, url: str = "https://www.monsterhunter.com/20th/en/vote-monster/result/") -> None:
-        self.url = url
-        self.headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"}
+    RANKING_URL = r"https://www.monsterhunter.com/20th/en/vote-monster/result/"
 
-    def scrape(self):
+    DATA_PATH = AbstractWebScraper.DATA_PATH / "subsets" / "general"
+    DEFAULT_PARTIAL_RANKING_PATH = DATA_PATH / "partial" / "partial_ranking_data.json"
+    DEFAULT_RANKING_RESULTS_PATH = DATA_PATH / "ranking_data.csv"
+
+    @property
+    def top_3(self) -> List[RankingScraperItem]:
+        return self._get_top_3()
+
+    @cached_property
+    @file_cache("DEFAULT_PARTIAL_RANKING_PATH")
+    def ranks_4_to_228(self) -> List[RankingScraperItem]:
+        return self._get_4_to_228()
+
+    def scrape(self) -> List[RankingScraperItem]:
         logger.info("Start scraping Official Capcom Fan Ranking.")
-
         monster_rankings = []
 
-        soup = self.retrieve_soup()
-
-        monster_rankings.extend(self.get_top_3())
-        monster_rankings.extend(self.get_4_to_228(soup))
-
-        logger.info(f"All Monster Ranks successfully scraped! {len(monster_rankings)} items scraped in total!")
+        monster_rankings.extend(self.top_3)
+        monster_rankings.extend(self.ranks_4_to_228)
 
         return monster_rankings
     
-    def get_top_3(self) -> List[RankingScraperItem]:
-        top_3 = []
-        top_3_data = [
+    def _get_top_3(self) -> List[RankingScraperItem]:
+        top_3 = [
             {"monster_name": "Zinogre", "rank": 1},
             {"monster_name": "Nergigante", "rank": 2},
             {"monster_name": "Lagiacrus", "rank": 3}
             ]
-        
-        for monster in top_3_data:
-            top_3.append(RankingScraperItem(**monster))
 
-        logger.info("Top 3 retrieved.")
-
-        return top_3
+        return [RankingScraperItem(**entry) for entry in top_3]
     
-    def get_4_to_228(self, soup: BeautifulSoup) -> List[RankingScraperItem]:
+    def _get_4_to_228(self) -> List[RankingScraperItem]:
         top_4_to_bottom = []
 
-        ranking = soup.find('div', class_ = 'ranking')
+        soup = self.retrieve_soup(self.RANKING_URL)
+        ranking = soup.find('div', class_= 'ranking')
 
         li_top_20_tags = ranking.find_all('li', class_ = 'no-4-18')
         li_bottom_tags = ranking.find_all('li', class_ = 'no-img')
@@ -62,13 +65,11 @@ class RankingScraper(AbstractWebScraper[RankingScraperItem]):
                 rank = int(rank_div.text.split('.')[-1].strip())
 
                 rank_dict = {"monster_name": name, "rank": rank}
-                rank_dict = RankingScraperItem(**rank_dict)
-                
-                top_4_to_bottom.append(rank_dict)
+
+                top_4_to_bottom.append(RankingScraperItem(**rank_dict))
 
             except AttributeError:
                 logger.warning(f"No text for scraped item Nr. {rank} found!")
 
         logger.info(f"Ranks 4 to 229 successfully scraped! {len(top_4_to_bottom)} items scraped.")
-
         return top_4_to_bottom
